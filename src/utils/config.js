b98d90eed16cc6c30b1405a6456ed1f7f751d2a7
houser <houserqu@qq.com>
2017-12-27 14:33:41 +0800

    修改为https

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,14 +1,14 @@
-export const sitename='经英教育';
+export const sitename = '经英教育';
 
-export const API_DOMAIN = 'http://jywxsitex.sinogood.com/';
+export const API_DOMAIN = 'https://jywxsitex.sinogood.com/';
 // export const API_DOMAIN = 'http://106.14.112.47/';
 // export const API_DOMAIN = 'http://172.25.253.8/';
 
-export const IMG_DOMAIN = 'http://jywxsitex.sinogood.com/';
+export const IMG_DOMAIN = 'https://jywxsitex.sinogood.com/';
 
-export const SCH_BADGE = 'http://jywxsitex.sinogood.com/';
+export const SCH_BADGE = 'https://jywxsitex.sinogood.com/';
 
-export const WECHAT_LOGIN = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx8032839074009dce&redirect_uri=http://jywxsitex.sinogood.com/pub/member/weichatAuth/callBack&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
+export const WECHAT_LOGIN = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx8032839074009dce&redirect_uri=https://jywxsitex.sinogood.com/pub/member/weichatAuth/callBack&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
 
 export const DELIVER = {
 };
